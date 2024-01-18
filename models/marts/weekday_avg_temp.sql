db728aab0b39d95b5f5748ed9f1586c70e084dbe
ValeMari <valerio.marinone.vb@gmail.com>
2024-01-18 13:19:59 +0100

    avg month

--- models/marts/weekday_avg_temp.sql
+++ models/marts/weekday_avg_temp.sql
@@ -1,11 +1,12 @@
 WITH tot_avg_month AS (
     SELECT 
     city, country, year, month, lat, lon,
-    avg(avgtemp_c) as avg_temp_month,
+    round(avg(avgtemp_c),2) as avg_temp_month,
     max(maxtemp_c) as max_temp_month, 
     min(mintemp_c) as min_temp_month
     from {{ref("prep_temp")}}
     group by city, country, year, month, lat, lon
+    order by city, month
 )
 
 SELECT *
