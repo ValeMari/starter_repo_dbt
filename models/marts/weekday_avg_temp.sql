060bc6fcfdc69a36630c0ffb753f07cd6083d8d7
ValeMari <valerio.marinone.vb@gmail.com>
2024-01-18 11:06:04 +0100

    Thursday_challenge

---
+++ models/marts/weekday_avg_temp.sql
@@ -0,0 +1,11 @@
+WITH tot_avg_month AS (
+    SELECT * 
+    FROM city, country, year, lat, lon,
+    avg(avgtemp_c) as avg_temp_month,
+    max(maxtemp_c) as max_temp_month, min(mintemp_c) as min_temp_month,
+    from {{ref('prep_temp')}}
+    group by city, country, year, month, lat, lon
+),
+
+SELECT *
+FROM total_avg_month
