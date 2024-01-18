b032a6340b97e25bdffbd87c70b1c9a030081b1e
ValeMari <valerio.marinone.vb@gmail.com>
2024-01-18 13:18:04 +0100

    monthly_avg updated

--- models/marts/weekday_avg_temp.sql
+++ models/marts/weekday_avg_temp.sql
@@ -1,6 +1,6 @@
 WITH tot_avg_month AS (
     SELECT 
-    city, country, year, lat, lon,
+    city, country, year, month, lat, lon,
     avg(avgtemp_c) as avg_temp_month,
     max(maxtemp_c) as max_temp_month, 
     min(mintemp_c) as min_temp_month
