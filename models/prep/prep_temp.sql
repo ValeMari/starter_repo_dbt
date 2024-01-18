59552eb488c5628265de2e226397e8f938730ead
ValeMari <valerio.marinone.vb@gmail.com>
2024-01-18 12:44:35 +0100

    fixed prep file

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -5,7 +5,9 @@ WITH temp_daily AS (
 add_weekday AS (
     SELECT *,
         date_part('day',date) AS weekday,
-        date_part('dow',date) AS day_num
+        date_part('dow',date) AS day_num,
+        date_part('week',date) AS week,
+        date_part('month',date) AS month
     FROM temp_daily
 )
 SELECT *
